06b09f35392abf1dc698de2e64a140ac2a5285d2
arsewisely <43777639+arsewisely@users.noreply.github.com>
2018-10-02 17:38:20 +0800

    dot points in the comment

--- base/logging.jl
+++ base/logging.jl
@@ -31,10 +31,10 @@ abstract type AbstractLogger ; end
 """
     handle_message(logger, level, message, _module, group, id, file, line; key1=val1, ...)
 
-Log a message to `logger` at `level`.  The logical location at which the
-message was generated is given by module `_module` and `group`; the source
-location by `file` and `line`. `id` is an arbitrary unique `Symbol` to be used
-as a key to identify the log statement when filtering.
+ * Log a message to `logger` at `level`.  
+ * The logical location at which the message was generated is given by module `_module` and `group`, 
+ * the source location by `file` and `line`
+ * `id` is an arbitrary unique `Symbol` to be used as a key to identify the log statement when filtering.
 """
 function handle_message end
 
